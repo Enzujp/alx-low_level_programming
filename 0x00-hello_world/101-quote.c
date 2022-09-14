3318b331567d7a4b94298e1d4d5049c7d676e25c
Enzujp <100221308+Enzujp@users.noreply.github.com>
2022-09-14 14:17:46 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - program execution begins here
- *
- *   * Description: this is the main function
- *
- *    * Return: an integer to signify success or failure
- *
- *     */
-
+ * main - program execution begins here
+ * Description: this is the main function
+ * Return: an integer to signify success or failure
+ */
 int main(void)
-
 {
-
-		fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-
-					59, 1, stderr);
-
-			return (1);
-
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59, 1, stderr);
+	return (1);
 }
