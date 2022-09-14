04c7d8954f2ba1c7f38a5a2ee40d00b2953ea11a
Okache Johnpaul <johnpaulokache@gmail.com>
2022-09-14 05:38:03 -0700

    well well well

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - program execution begins here
+ *
+ *   * Description: this is the main function?
+ *
+ *    * Return: gives back an integer which signifies success or fail
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
