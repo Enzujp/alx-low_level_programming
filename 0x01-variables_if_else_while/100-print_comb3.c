969452af875e8acb3477033ef827a68441f10294
Enzujp <100221308+Enzujp@users.noreply.github.com>
2022-09-14 14:42:34 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - program execution starts here
+ * Description: this is the main function
+ * Return: an integer to signify success or failure
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (!(j <= i))
+			{
+				putchar(i);
+				putchar(j);
+				if (!(i == '8' && j == '9'))
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+Foo
