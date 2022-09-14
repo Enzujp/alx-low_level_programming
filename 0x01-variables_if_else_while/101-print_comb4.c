e5dc3c0089dc0f49126fb9f7fce6d6e5ff948858
Enzujp <100221308+Enzujp@users.noreply.github.com>
2022-09-14 14:43:23 +0100

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - program execution starts here
+ * Description: this is the main function
+ * Return: an integer to signify success or failure
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if ((i < j && j < k) && (i != j && j != k && k != i))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+Foo
